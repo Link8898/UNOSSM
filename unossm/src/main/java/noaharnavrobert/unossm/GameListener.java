0fbcb66e0a371ea38368d01ad37757febf725fbd
Robert Burdsall <burdsall25r@ncssm.edu>
2024-05-17 17:39:06 -0400

    Update GameListener.java

--- unossm/src/main/java/noaharnavrobert/unossm/GameListener.java
+++ unossm/src/main/java/noaharnavrobert/unossm/GameListener.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class GameListener extends Thread{
 
@@ -30,7 +31,10 @@ public class GameListener extends Thread{
         }
     }
 
-    public void GetHand(int id) {
+    public ArrayList<String> GetHand(int id) {
+
+        // client requests its hand
+        // server return logic.getHand()
 
     }
 
