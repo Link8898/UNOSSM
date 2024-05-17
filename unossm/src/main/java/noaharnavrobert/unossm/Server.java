77f6e3a3b6e19bf262e567cad407cb8d89479a30
Robert Burdsall <burdsall25r@ncssm.edu>
2024-05-17 18:35:35 -0400

    Update Server.java

--- unossm/src/main/java/noaharnavrobert/unossm/Server.java
+++ unossm/src/main/java/noaharnavrobert/unossm/Server.java
@@ -125,7 +125,7 @@ public class Server extends Thread {
     public void game(){
         logic = new Logic(players.size());
         try {
-            ServerSocket serverSocket = new ServerSocket(1234);
+            ServerSocket serverSocket = new ServerSocket(5678);
             System.out.println("TCP Listening on port 1234");
             while(running) {
                 Socket clientSocket = serverSocket.accept();
@@ -145,7 +145,7 @@ public class Server extends Thread {
                 } else if (split_msg[0].equals("GetID")) {
                     int id = playerips.indexOf(split_msg[1]);
                     out.println(id);
-                } 
+                }
 
                 //out.println("True, Noah really does suck!!!");
             }
