fffd9b05646c06bf1517a4dcd8f3b2396505ddbd
Robert Burdsall <burdsall25r@ncssm.edu>
2024-05-17 22:18:23 -0400

    Update Server.java

--- unossm/src/main/java/noaharnavrobert/unossm/Server.java
+++ unossm/src/main/java/noaharnavrobert/unossm/Server.java
@@ -1,4 +1,34 @@
 package noaharnavrobert.unossm;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String args[]) throws IOException 
+    {
+        // create a server socket on port number 9090
+        ServerSocket serverSocket = new ServerSocket(6969);
+        System.out.println("Server searching...");
+
+        // Accept incoming client connection
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected from ip "+clientSocket.getInetAddress());
+
+        // Setup input and output streams for communication with the client
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+        // Read message from client
+        String message = in.readLine();
+        System.out.println("Client says: " + message);
+
+        // Send response to the client
+        Scanner scanner = new Scanner(System.in);
+        String msg = scanner.next();
+        out.println(msg);
+
+        // Close the client socket
+        // Close the server socket
+    }
 }
